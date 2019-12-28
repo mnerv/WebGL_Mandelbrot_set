48cbe07afe55a6b632560d6730be7926fb697a99
KTheXIII <24420859+KTheXIII@users.noreply.github.com>
2019-12-28 20:20:27 +0100

    interface, styles, color changing fragment, smooth gradient
    Added colors to the mandelbrot set, symmetry, max iterationen variable.
    
    User interface and styling implemented.

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # Mandelbrot Set
+
+Mandelbrot set, one of the coolast thing ever discovered. This is my version using WebGL.
--- scripts/sketch.js
+++ scripts/sketch.js
@@ -1,22 +1,37 @@
 let running = true
 
 // html elements
+let cnv
 let loopEl
 let locationEl
 let setBtnEl
+let maxiterEl, maxradEl
+let symEl
+let cRangeEl
 
 // a shader variable
 let theShader
 
 let navValue
+let navSmooth
 let activeNav
+let coffset
 
+let smoothStep = 0.1
 let baseStep = 0.05
 let navStep = baseStep
 let scaleStep = 0.01
 let rotationStep
 
+const MAX_ITER = 30000
+let maxIteration = 255
+let maxRadius = 10
+let symmetry = 0
+
 let interfaceSelected = false
+let enableControl = false
+
+let totalKeyPress = 0
 
 function preload() {
   // load the shader
@@ -24,8 +39,6 @@ function preload() {
 }
 
 function setup() {
-  initElements()
-
   // Assign values
   navValue = {
     x: 0,
@@ -34,6 +47,7 @@ function setup() {
     w: 2.35,
     a: 0
   }
+  navSmooth = Object.assign({}, navValue)
   activeNav = {
     x: 0,
     y: 0,
@@ -41,20 +55,34 @@ function setup() {
     w: 0,
     a: 0
   }
+  coffset = {
+    r: 0.3,
+    g: 0.4,
+    b: 0.65,
+    w: 50
+  }
+  initElements()
   rotationStep = PI / 180
 
   updateLocationEl()
-
-  // navValue.x = -1.25066
-  // navValue.y = 0.02012
-
   // shaders require WEBGL mode to work
-  createCanvas(windowWidth, windowHeight, WEBGL)
+  cnv = createCanvas(windowWidth, windowHeight, WEBGL)
+  cnv.mouseWheel(zoomScroll)
+  cnv.mouseOver(() => {
+    enableControl = true
+  })
+  cnv.mouseOut(() => {
+    enableControl = false
+    // resetValues()
+    // hardResetValue()
+  })
+  // cnv.keyPressed(cnvKeyPre)
+  // cnv.keyReleased(cnvKeyRel)
+
   noStroke()
 
   shader(theShader)
   theShader.setUniform('u_resolution', [width, height])
-
   rect(0, 0, width, height)
   updateShader()
 }
@@ -64,18 +92,30 @@ function draw() {
   updateShader()
 
   updateValues()
+  if (totalKeyPress == 0) hardResetValue()
 }
 
 function updateShader() {
   shader(theShader)
 
-  theShader.setUniform('u_Area', [
-    navValue.x,
-    navValue.y,
-    navValue.z,
-    navValue.w
+  theShader.setUniform('u_cOffset', [
+    coffset.r,
+    coffset.g,
+    coffset.b,
+    coffset.w
+  ])
+  theShader.setUniform('u_symmetry', symmetry)
+  theShader.setUniform('u_maxIter', maxIteration)
+  theShader.setUniform('u_radius', maxRadius)
+  theShader.setUniform('u_time', millis() / 1000)
+
+  theShader.setUniform('u_area', [
+    navSmooth.x,
+    navSmooth.y,
+    navSmooth.z,
+    navSmooth.w
   ]) // xy:location and zw:zoom
-  theShader.setUniform('u_Angle', navValue.a) // rect gives us some geometry on the screen
+  theShader.setUniform('u_angle', navSmooth.a) // rect gives us some geometry on the screen
 }
 
 function updateValues() {
@@ -85,10 +125,17 @@ function updateValues() {
 
   navValue.x += dir.x * c - dir.y * s
   navValue.y += dir.x * s + dir.y * c
-
   navValue.z += activeNav.z
   navValue.w += activeNav.w
   navValue.a += activeNav.a
+
+  navSmooth.x = lerp(navSmooth.x, navValue.x, smoothStep)
+  navSmooth.y = lerp(navSmooth.y, navValue.y, smoothStep)
+
+  navSmooth.z = lerp(navSmooth.z, navValue.z, smoothStep)
+  navSmooth.w = lerp(navSmooth.w, navValue.w, smoothStep)
+
+  navSmooth.a = lerp(navSmooth.a, navValue.a, smoothStep)
 
   if (activeNav.x != 0 || activeNav.y != 0) {
     updateLocationEl()
@@ -128,6 +175,8 @@ function hardResetValue() {
   activeNav.z = 0
   activeNav.w = 0
   activeNav.a = 0
+
+  totalKeyPress = 0
 }
 
 function navigate(key) {
@@ -156,9 +205,49 @@ function navigate(key) {
   }
 }
 
+function zoomScroll(event) {
+  if (running) {
+    navValue.z += event.deltaY / 1000
+    navValue.w += event.deltaY / 1000
+  }
+}
+
+function setMaxIter(event) {
+  if (event.type === 'wheel') {
+    maxIteration += event.deltaY
+    if (maxIteration < 1) maxIteration = 1
+    else if (maxIteration > MAX_ITER) maxIteration = MAX_ITER
+  } else if (event === 'changed') {
+  }
+
+  maxiterEl.value(maxIteration)
+}
+
+function setMaxRad(event) {
+  if (event.type === 'wheel') {
+    maxRadius += event.deltaY
+    if (maxRadius < 1) maxRadius = 1
+  } else if (event === 'changed') {
+  }
+
+  maxradEl.value(maxRadius)
+}
+
 function setNavValue() {
-  navValue.x = locationEl.x.value()
-  navValue.y = locationEl.y.value()
+  if (!isNaN(locationEl.x.value() && !isNaN(locationEl.y.value()))) {
+    navValue.x = float(locationEl.x.value())
+    navValue.y = float(locationEl.y.value())
+  }
+}
+
+function setColorGradient() {
+  coffset.r = cRangeEl.r.value() / 100
+  coffset.g = cRangeEl.g.value() / 100
+  coffset.b = cRangeEl.b.value() / 100
+}
+
+function setSymmetry() {
+  symmetry = symEl.value() / 100
 }
 
 function initElements() {
@@ -172,11 +261,31 @@ function initElements() {
   }
   setBtnEl = select('#updateLocation')
   setBtnEl.mousePressed(setNavValue)
-}
 
-function mouseWheel(event) {
-  navValue.z += event.delta / 1000
-  navValue.w += event.delta / 1000
+  maxiterEl = select('#maxiter')
+  maxiterEl.mouseWheel(setMaxIter)
+  maxiterEl.changed(setMaxIter)
+
+  maxradEl = select('#maxrad')
+  maxradEl.mouseWheel(setMaxRad)
+  maxradEl.changed(setMaxRad)
+
+  symEl = select('#syminput')
+  symEl.mouseMoved(setSymmetry)
+  symEl.changed(setSymmetry)
+
+  cRangeEl = {
+    r: select('#redinput'),
+    g: select('#greeninput'),
+    b: select('#blueinput')
+  }
+  cRangeEl.r.value(coffset.r * 100)
+  cRangeEl.g.value(coffset.g * 100)
+  cRangeEl.b.value(coffset.b * 100)
+
+  cRangeEl.r.mouseMoved(setColorGradient)
+  cRangeEl.g.mouseMoved(setColorGradient)
+  cRangeEl.b.mouseMoved(setColorGradient)
 }
 
 function windowResized() {
@@ -186,11 +295,14 @@ function windowResized() {
 
 function keyPressed() {
   if (keyCode == 32) playpause()
-  navigate(key)
+  if (enableControl) navigate(key)
+  totalKeyPress++
 }
 
 function keyReleased() {
+  if (enableControl) navigate(key)
   resetValues()
+  totalKeyPress--
 }
 
 function playpause() {
